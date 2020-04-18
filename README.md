d54cc1f80c4f0ab56246807ada6725e12cabc270
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-04-18 14:11:59 +0900

    initial release

---
+++ README.md
@@ -0,0 +1,16 @@
+# juman-cygwin64
+
+[JUMAN](http://nlp.ist.i.kyoto-u.ac.jp/?JUMAN) 7.01 for [Cygwin64](https://www.cygwin.com/).
+
+## Usage
+
+See [JUMAN page](http://nlp.ist.i.kyoto-u.ac.jp/?JUMAN).
+
+## Installation
+
+Only for Cygwin64 with `python37-pip` and `git`:
+
+```sh
+pip3.7 install git+https://github.com/KoichiYasuoka/juman-cygwin64
+```
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import glob
+import platform
+if platform.system().startswith("CYGWIN") and platform.machine()=="x86_64":
+  pass
+else:
+  raise OSError("juman-cygwin64 only for 64-bit Cygwin")
+
+setuptools.setup(
+  name="juman-cygwin64",
+  version="0.2.0",
+  packages=setuptools.find_packages(),
+  data_files=[
+    ("local/bin",glob.glob("bin/*")),
+    ("local/libexec/juman",glob.glob("libexec/juman/*")),
+    ("local/lib",glob.glob("lib/*")),
+    ("local/etc",glob.glob("etc/*")),
+    ("local/include",glob.glob("include/*")),
+    ("local/share/juman/dic",glob.glob("share/juman/dic/*")),
+    ("local/share/juman/autodic",glob.glob("share/juman/autodic/*")),
+    ("local/share/juman/wikipediadic",glob.glob("share/juman/wikipediadic/*")),
+    ("local/share/juman/doc",glob.glob("share/juman/doc/*"))
+  ]
+)
